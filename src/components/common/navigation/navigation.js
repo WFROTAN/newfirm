94fa52da4926206dc40cafeba53c6a4444fe3cc2
WFROTAN <wfrotan@myseneca.ca>
2021-04-26 18:02:30 -0400

    added portfolio

--- src/components/common/navigation/navigation.js
+++ src/components/common/navigation/navigation.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import { Link } from "gatsby"
 import AnchorLink from "react-anchor-link-smooth-scroll"
 import Scrollspy from "react-scrollspy"
 import { Menu, X } from "react-feather"
@@ -17,7 +18,7 @@ import {
   ActionsContainer,
 } from "./style"
 
-const NAV_ITEMS = ["Features", "About", "Pricing", "Contact", ""]
+const NAV_ITEMS = ["Features", "About", "Pricing", "Contact"]
 
 export default class Navigation extends Component {
   state = {
@@ -57,6 +58,12 @@ export default class Navigation extends Component {
     </AnchorLink>
   )
 
+  getNavLink = (item) => (
+    <Link href={`/#${item.toLowerCase()}`} onClick={this.closeMobileMenu}>
+      {item}
+    </Link>
+  )
+
   getNavList = ({ mobile = false }) => (
     <NavListWrapper mobile={mobile}>
       <Scrollspy
@@ -68,52 +75,118 @@ export default class Navigation extends Component {
         {NAV_ITEMS.map((navItem) => (
           <NavItem key={navItem}>{this.getNavAnchorLink(navItem)}</NavItem>
         ))}
+        <NavItem>
+          <Link href="/portfolio">Portfolio</Link>
+        </NavItem>
+      </Scrollspy>
+    </NavListWrapper>
+  )
+
+  getNavLinkList = ({ mobile = false }) => (
+    <NavListWrapper mobile={mobile}>
+      <Scrollspy
+        items={NAV_ITEMS.map((item) => item.toLowerCase())}
+        currentClassName="active"
+        mobile={mobile}
+        offset={-64}
+      >
+        {NAV_ITEMS.map((navItem) => (
+          <NavItem key={navItem}>{this.getNavLink(navItem)}</NavItem>
+        ))}
+        <NavItem>
+          <Link href="/portfolio">Portfolio</Link>
+        </NavItem>
       </Scrollspy>
     </NavListWrapper>
   )
 
   render() {
     const { mobileMenuOpen } = this.state
+    const url = typeof window !== "undefined" ? window.location.href : ""
+    console.log(url)
 
-    return (
-      <Nav {...this.props} scrolled={this.state.hasScrolled}>
-        <StyledContainer>
-          <Brand>
-            <Scrollspy offset={-64} item={["top"]} currentClassName="active">
-              <AnchorLink href="#top" onClick={this.closeMobileMenu}>
-                <div className="logo-container">
-                  <LogoRocket />{" "}
-                  <div className="logo-text">
-                    <span>AFTER</span>&nbsp;RED
+    if (url === "http://localhost:8000") {
+      return (
+        <Nav {...this.props} scrolled={this.state.hasScrolled}>
+          <StyledContainer>
+            <Brand>
+              <Scrollspy offset={-64} item={["top"]} currentClassName="active">
+                <AnchorLink href="#top" onClick={this.closeMobileMenu}>
+                  <div className="logo-container">
+                    <LogoRocket />{" "}
+                    <div className="logo-text">
+                      <span>AFTER</span>&nbsp;RED
+                    </div>
                   </div>
-                </div>
-              </AnchorLink>
-            </Scrollspy>
-          </Brand>
-          <Mobile>
-            <button
-              onClick={this.toggleMobileMenu}
-              style={{ color: "black", background: "none" }}
-            >
-              {this.state.mobileMenuOpen ? (
-                <X size={24} alt="close menu" />
-              ) : (
-                <Menu size={24} alt="open menu" />
-              )}
-            </button>
-          </Mobile>
+                </AnchorLink>
+              </Scrollspy>
+            </Brand>
+            <Mobile>
+              <button
+                onClick={this.toggleMobileMenu}
+                style={{ color: "black", background: "none" }}
+              >
+                {this.state.mobileMenuOpen ? (
+                  <X size={24} alt="close menu" />
+                ) : (
+                  <Menu size={24} alt="open menu" />
+                )}
+              </button>
+            </Mobile>
 
-          <Mobile hide>{this.getNavList({})}</Mobile>
-          <ActionsContainer>(647) 271-2701</ActionsContainer>
-        </StyledContainer>
-        <Mobile>
-          {mobileMenuOpen && (
-            <MobileMenu>
-              <Container>{this.getNavList({ mobile: true })}</Container>
-            </MobileMenu>
-          )}
-        </Mobile>
-      </Nav>
-    )
+            <Mobile hide>{this.getNavList({})}</Mobile>
+            <ActionsContainer>(647) 271-2701</ActionsContainer>
+          </StyledContainer>
+          <Mobile>
+            {mobileMenuOpen && (
+              <MobileMenu>
+                <Container>{this.getNavList({ mobile: true })}</Container>
+              </MobileMenu>
+            )}
+          </Mobile>
+        </Nav>
+      )
+    } else {
+      return (
+        <Nav {...this.props} scrolled={this.state.hasScrolled}>
+          <StyledContainer>
+            <Brand>
+              <Scrollspy offset={-64} item={["top"]} currentClassName="active">
+                <Link href="/" onClick={this.closeMobileMenu}>
+                  <div className="logo-container">
+                    <LogoRocket />{" "}
+                    <div className="logo-text">
+                      <span>AFTER</span>&nbsp;RED
+                    </div>
+                  </div>
+                </Link>
+              </Scrollspy>
+            </Brand>
+            <Mobile>
+              <button
+                onClick={this.toggleMobileMenu}
+                style={{ color: "black", background: "none" }}
+              >
+                {this.state.mobileMenuOpen ? (
+                  <X size={24} alt="close menu" />
+                ) : (
+                  <Menu size={24} alt="open menu" />
+                )}
+              </button>
+            </Mobile>
+
+            <Mobile hide>{this.getNavLinkList({})}</Mobile>
+            <ActionsContainer>(647) 271-2701</ActionsContainer>
+          </StyledContainer>
+          <Mobile>
+            {mobileMenuOpen && (
+              <MobileMenu>
+                <Container>{this.getNavLinkList({ mobile: true })}</Container>
+              </MobileMenu>
+            )}
+          </Mobile>
+        </Nav>
+      )
+    }
   }
 }
---
+++ src/components/sections/portfolio.js
@@ -0,0 +1,122 @@
+import React from "react"
+import styled from "styled-components"
+
+import { Section, Container } from "../global"
+
+const Portfolio = () => (
+  <StyledSection>
+    <PortfolioText>
+      <TextContent>
+        <h2>Our Portfolio</h2>
+        <p>
+          Curious what we did with others? See some of our (not under NDA)
+          works.
+        </p>
+      </TextContent>
+    </PortfolioText>
+    <PortfolioContainer>
+      <PortfolioItems>
+        <PortfolioRow>
+          <PortfolioItem href="https://flatworld.co/">
+            <h4>Flat World</h4>
+            <p>Going JAMstack with Hassle-free WordPress and Gatsby.</p>
+          </PortfolioItem>
+          <PortfolioItem href="https://autoloadit.com/">
+            <h4>AutoloadIT</h4>
+            <p>Blazing Fast, SEO friendly eCommerce for Anime fans.</p>
+          </PortfolioItem>
+          <PortfolioItem href="https://lbifinancial.com/">
+            <h4>LBI Financial</h4>
+            <p>The fastest insurance business website in NYC.</p>
+          </PortfolioItem>
+        </PortfolioRow>
+      </PortfolioItems>
+    </PortfolioContainer>
+  </StyledSection>
+)
+
+export default Portfolio
+
+const StyledSection = styled(Section)`
+  margin-top: 100px;
+`
+
+const PortfolioText = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 20px;
+`
+
+const TextContent = styled.div`
+  text-align: center;
+`
+
+const PortfolioContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`
+
+const PortfolioItems = styled.div`
+  width: 1000px;
+`
+
+const PortfolioRow = styled.div`
+  display: flex;
+  margin-top: 10px;
+
+  a {
+    text-decoration: none;
+    color: #222;
+  }
+
+  a:hover {
+    background-color: #ff4432;
+  }
+`
+
+const PortfolioItem = styled.a`
+  width: calc(33.333333% - 20px);
+  margin-left: 10px;
+  margin-right: 10px;
+  width: 100%;
+  height: 100%;
+  border: 1px solid #222;
+  padding: 0 30px;
+  padding-bottom: 25px;
+  text-align: center;
+`
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`
+
+const PortfolioButton = styled.button`
+  margin-top: 40px;
+  margin-bottom: 40px;
+  font-weight: 500;
+  font-size: 14px;
+  color: white;
+  letter-spacing: 1px;
+  height: 60px;
+  display: block;
+  text-transform: uppercase;
+  cursor: pointer;
+  white-space: nowrap;
+  background: #ff4432;
+  border-radius: 4px;
+  padding: 0px 40px;
+  border-width: 0px;
+  border-style: initial;
+  border-color: initial;
+  border-image: initial;
+  outline: 0px;
+  &:hover {
+    box-shadow: rgba(110, 120, 152, 0.22) 0px 2px 10px 0px;
+  }
+  @media (max-width: ${(props) => props.theme.screen.md}) {
+  }
+  @media (max-width: ${(props) => props.theme.screen.sm}) {
+    margin-left: 0;
+  }
+`

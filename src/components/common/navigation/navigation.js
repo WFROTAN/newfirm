fbcd3a943784854872f1b27907bcba8d7dd4104c
WFROTAN <wfrotan@myseneca.ca>
2021-05-01 12:12:01 -0400

    fixed broken links v1

--- src/components/common/navigation/navigation.js
+++ src/components/common/navigation/navigation.js
@@ -105,48 +105,7 @@ export default class Navigation extends Component {
     const url = typeof window !== "undefined" ? window.location.href : ""
     console.log(url)
 
-    if (url === "https://www.after-red.com/") {
-      return (
-        <Nav {...this.props} scrolled={this.state.hasScrolled}>
-          <StyledContainer>
-            <Brand>
-              <Scrollspy offset={-64} item={["top"]} currentClassName="active">
-                <AnchorLink href="#top" onClick={this.closeMobileMenu}>
-                  <div className="logo-container">
-                    <LogoRocket />{" "}
-                    <div className="logo-text">
-                      <span>AFTER</span>&nbsp;RED
-                    </div>
-                  </div>
-                </AnchorLink>
-              </Scrollspy>
-            </Brand>
-            <Mobile>
-              <button
-                onClick={this.toggleMobileMenu}
-                style={{ color: "black", background: "none" }}
-              >
-                {this.state.mobileMenuOpen ? (
-                  <X size={24} alt="close menu" />
-                ) : (
-                  <Menu size={24} alt="open menu" />
-                )}
-              </button>
-            </Mobile>
-
-            <Mobile hide>{this.getNavList({})}</Mobile>
-            <ActionsContainer>(647) 271-2701</ActionsContainer>
-          </StyledContainer>
-          <Mobile>
-            {mobileMenuOpen && (
-              <MobileMenu>
-                <Container>{this.getNavList({ mobile: true })}</Container>
-              </MobileMenu>
-            )}
-          </Mobile>
-        </Nav>
-      )
-    } else {
+    if (url === "https://www.after-red.com/portfolio/") {
       return (
         <Nav {...this.props} scrolled={this.state.hasScrolled}>
           <StyledContainer>
@@ -182,6 +141,47 @@ export default class Navigation extends Component {
             {mobileMenuOpen && (
               <MobileMenu>
                 <Container>{this.getNavLinkList({ mobile: true })}</Container>
+              </MobileMenu>
+            )}
+          </Mobile>
+        </Nav>
+      )
+    } else {
+      return (
+        <Nav {...this.props} scrolled={this.state.hasScrolled}>
+          <StyledContainer>
+            <Brand>
+              <Scrollspy offset={-64} item={["top"]} currentClassName="active">
+                <AnchorLink href="#top" onClick={this.closeMobileMenu}>
+                  <div className="logo-container">
+                    <LogoRocket />{" "}
+                    <div className="logo-text">
+                      <span>AFTER</span>&nbsp;RED
+                    </div>
+                  </div>
+                </AnchorLink>
+              </Scrollspy>
+            </Brand>
+            <Mobile>
+              <button
+                onClick={this.toggleMobileMenu}
+                style={{ color: "black", background: "none" }}
+              >
+                {this.state.mobileMenuOpen ? (
+                  <X size={24} alt="close menu" />
+                ) : (
+                  <Menu size={24} alt="open menu" />
+                )}
+              </button>
+            </Mobile>
+
+            <Mobile hide>{this.getNavList({})}</Mobile>
+            <ActionsContainer>(647) 271-2701</ActionsContainer>
+          </StyledContainer>
+          <Mobile>
+            {mobileMenuOpen && (
+              <MobileMenu>
+                <Container>{this.getNavList({ mobile: true })}</Container>
               </MobileMenu>
             )}
           </Mobile>

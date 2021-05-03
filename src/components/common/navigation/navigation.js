f625f2d347391f84f33e78d83695bc8b276a470b
WFROTAN <wfrotan@myseneca.ca>
2021-05-03 18:05:43 -0400

    fixed broken links v12

--- src/components/common/navigation/navigation.js
+++ src/components/common/navigation/navigation.js
@@ -104,7 +104,7 @@ export default class Navigation extends Component {
     const { mobileMenuOpen } = this.state
     const url = typeof window !== "undefined" ? window.location.href : ""
     console.log(url)
-    /*
+
     if (url !== "https://www.after-red.com/") {
       console.log("test")
       return (
@@ -148,48 +148,47 @@ export default class Navigation extends Component {
         </Nav>
       )
     } else {
-      */
-    return (
-      <Nav {...this.props} scrolled={this.state.hasScrolled}>
-        <StyledContainer>
-          <Brand>
-            <Scrollspy offset={-64} item={["top"]} currentClassName="active">
-              <AnchorLink href="#top" onClick={this.closeMobileMenu}>
-                <div className="logo-container">
-                  <LogoRocket />{" "}
-                  <div className="logo-text">
-                    <span>AFTER</span>&nbsp;RED
+      return (
+        <Nav {...this.props} scrolled={this.state.hasScrolled}>
+          <StyledContainer>
+            <Brand>
+              <Scrollspy offset={-64} item={["top"]} currentClassName="active">
+                <AnchorLink href="#top" onClick={this.closeMobileMenu}>
+                  <div className="logo-container">
+                    <LogoRocket />{" "}
+                    <div className="logo-text">
+                      <span>AFTER</span>&nbsp;RED
+                    </div>
                   </div>
-                </div>
-              </AnchorLink>
-            </Scrollspy>
-          </Brand>
-          <Mobile>
-            <button
-              onClick={this.toggleMobileMenu}
-              style={{ color: "black", background: "none" }}
-            >
-              {this.state.mobileMenuOpen ? (
-                <X size={24} alt="close menu" />
-              ) : (
-                <Menu size={24} alt="open menu" />
-              )}
-            </button>
-          </Mobile>
+                </AnchorLink>
+              </Scrollspy>
+            </Brand>
+            <Mobile>
+              <button
+                onClick={this.toggleMobileMenu}
+                style={{ color: "black", background: "none" }}
+              >
+                {this.state.mobileMenuOpen ? (
+                  <X size={24} alt="close menu" />
+                ) : (
+                  <Menu size={24} alt="open menu" />
+                )}
+              </button>
+            </Mobile>
 
-          <Mobile hide>{this.getNavList({})}</Mobile>
-          <ActionsContainer>(647) 271-2701</ActionsContainer>
-        </StyledContainer>
-        <Mobile>
-          {mobileMenuOpen && (
-            <MobileMenu>
-              <Container>{this.getNavList({ mobile: true })}</Container>
-            </MobileMenu>
-          )}
-        </Mobile>
-      </Nav>
-    )
+            <Mobile hide>{this.getNavList({})}</Mobile>
+            <ActionsContainer>(647) 271-2701</ActionsContainer>
+          </StyledContainer>
+          <Mobile>
+            {mobileMenuOpen && (
+              <MobileMenu>
+                <Container>{this.getNavList({ mobile: true })}</Container>
+              </MobileMenu>
+            )}
+          </Mobile>
+        </Nav>
+      )
+    }
   }
-  //}
 }
 //#${item.toLowerCase()}
